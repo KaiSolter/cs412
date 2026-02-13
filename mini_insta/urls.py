c1abc74062ff554bfc04c41cdca3c12e71c7cb31
Kai Solter <ksolter@bu.edu>
2026-02-13 00:23:00 -0500

    mini insta set up

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path
+from .views import ProfileListView, ProfileDetailView
 urlpatterns = [
-    
+    path('', ProfileListView.as_view() , name='profiles'),
+    path('profile/<int:pk>', ProfileDetailView.as_view(), name='profile' ),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView
+from .models import Profile
 # Create your views here.
+class ProfileListView(ListView):
+    '''
+    Display all profiles
+    '''
+    model = Profile
+    template_name = 'mini_insta/show_all_profiles.html'
+    context_object_name = 'profiles'
+    
+    
+class ProfileDetailView(DetailView):
+    '''
+    Display a single profile
+    '''
+    model = Profile
+    template_name = "mini_insta/show_profile.html"
+    context_object_name = 'profile'
